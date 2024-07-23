28e73dae688e07c78a534e3b08d410b2b4f9ac5b
Den Smalonski <dzianis.smalonski@protofire.io>
2024-07-23 12:40:40 +0200

    fix: updated default value for selector

--- src/hooks/useChainId.ts
+++ src/hooks/useChainId.ts
@@ -8,7 +8,7 @@ import { parsePrefixedAddress } from '@/utils/addresses'
 import useWallet from './wallets/useWallet'
 import useChains from './useChains'
 
-const defaultChainId = IS_PRODUCTION ? chains['scr'] : chains['scr-sepolia']
+const defaultChainId = IS_PRODUCTION ? chains['scr-sepolia'] : chains['scr-sepolia']
 
 // Use the location object directly because Next.js's router.query is available only on mount
 const getLocationQuery = (): ParsedUrlQuery => {
