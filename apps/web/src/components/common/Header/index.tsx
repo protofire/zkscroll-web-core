a9b49988c2a87065d2bc877151b836bd112548f2
ajimeno04 <jimenobeltran11@gmail.com>
2025-03-25 06:19:10 +0100

    refactor: Clean up Header component and update Help Center URLs
    - Commented out unused `useIsOfficialHost` hook in Header component.
    - Updated dApp exploration text for clarity in FirstSteps component.
    - Changed Help Center URLs to point to the new SAFE_HELP_CENTER_URL for consistency.

--- apps/web/src/components/common/Header/index.tsx
+++ apps/web/src/components/common/Header/index.tsx
@@ -24,7 +24,7 @@ import { useHasFeature } from '@/hooks/useChains'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS, OVERVIEW_LABELS } from '@/services/analytics'
 import { useSafeTokenEnabled } from '@/hooks/useSafeTokenEnabled'
-import { useIsOfficialHost } from '@/hooks/useIsOfficialHost'
+//import { useIsOfficialHost } from '@/hooks/useIsOfficialHost'
 import { BRAND_NAME } from '@/config/constants'
 
 type HeaderProps = {
@@ -47,7 +47,7 @@ const Header = ({ onMenuToggle, onBatchToggle }: HeaderProps): ReactElement => {
   const isSafeOwner = useIsSafeOwner()
   const router = useRouter()
   const enableWc = useHasFeature(FEATURES.NATIVE_WALLETCONNECT)
-  const isOfficialHost = useIsOfficialHost()
+  //const isOfficialHost = useIsOfficialHost()
 
   // If on the home page, the logo should link to the Accounts or Welcome page, otherwise to the home page
   const logoHref = getLogoLink(router)
@@ -80,7 +80,7 @@ const Header = ({ onMenuToggle, onBatchToggle }: HeaderProps): ReactElement => {
 
       <div className={classnames(css.element, css.logoMobile)}>
         <Link href={logoHref} passHref>
-          {isOfficialHost ? <SafeLogoMobile alt="Safe logo" /> : null}
+          <SafeLogoMobile alt="Safe logo" />
         </Link>
       </div>
 
