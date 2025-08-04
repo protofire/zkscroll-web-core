567ddddc596bc26efbc7008e10829ac910485a29
JohnPochta <johnpochta1@gmail.com>
2025-08-04 11:35:40 +0100

    feat(sunset): scroll sunset

--- apps/web/src/components/dashboard/index.tsx
+++ apps/web/src/components/dashboard/index.tsx
@@ -15,6 +15,7 @@ import { FEATURES } from '@/utils/chains'
 import css from './styles.module.css'
 import { InconsistentSignerSetupWarning } from '@/features/multichain/components/SignerSetupWarning/InconsistentSignerSetupWarning'
 import useIsStakingBannerEnabled from '@/features/stake/hooks/useIsStakingBannerEnabled'
+import { ZkScrollSunsetWarning } from '@/features/warningBanner/ScrollSunsetWarning'
 
 const RecoveryHeader = dynamic(() => import('@/features/recovery/components/RecoveryHeader'))
 
@@ -31,6 +32,10 @@ const Dashboard = (): ReactElement => {
 
         <Grid item xs={12}>
           <InconsistentSignerSetupWarning />
+        </Grid>
+
+        <Grid item xs={12}>
+          <ZkScrollSunsetWarning />
         </Grid>
 
         <Grid item xs={12}>
---
+++ apps/web/src/features/warningBanner/ScrollSunsetWarning.tsx
@@ -0,0 +1,29 @@
+import ErrorMessage from '@/components/tx/ErrorMessage'
+import { Typography } from '@mui/material'
+import ExternalLink from '@/components/common/ExternalLink'
+import { useHasFeature } from '@/hooks/useChains'
+import { FEATURES } from '@/utils/chains'
+
+export const ZkScrollSunsetWarning = () => {
+  const isFeatureEnabled = useHasFeature(FEATURES.ZKSCROLL_MAINNET_SUNSET_BANNER)
+
+  if (!isFeatureEnabled) return null
+
+  return (
+    <ErrorMessage level="warning" title="Safe on Scroll Sunsetting">
+      <Typography display="inline" mr={1}>
+        Dear users, Our Support for Safe on Scroll will end on August 31th, 2025.{' '}
+      </Typography>
+      <Typography display="inline">
+        You can proceed using Scroll through the {' '}
+        <ExternalLink href="https://app.safe.global/welcome">
+          Safe Global 
+        </ExternalLink>{' '}
+        and Scroll testnet through the{' '}
+        <ExternalLink href="https://docs.safe.global/advanced/cli-overview">
+          Safe CLI
+        </ExternalLink>{' '} utility
+      </Typography>
+    </ErrorMessage>
+  )
+}
